ce43ec6980dbb9429c62eea6b89c55ad29772a92
RobinKardell <robinkardell@Robins-MacBook-Air.local>
2021-06-21 16:35:43 +0200

    Small change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Deployment is happening here! 
+        Deployment is happening! 
         </p>
         <p>Contact me on robin@robinkardell.com</p>
       </header>
